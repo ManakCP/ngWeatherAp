08061cfbd2a9449831f6aa693fab966678ed765a
ManakCP <manakchand@outlook.com>
2020-02-07 16:33:33 +0530

    getWeatherByCity and getWeatherByCoords working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherService }  from './weather.service';
-import { Coord } from './Models/Coord';
-import { WeatherData } from './Models/WeatherData';
+import { Component, OnInit } from "@angular/core";
+import { WeatherService } from "./weather.service";
+import { Coord } from "./Models/Coord";
+import { WeatherData } from "./Models/WeatherData";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent implements OnInit  {
-  filterText:String;
-  weatherData:WeatherData;
+export class AppComponent implements OnInit {
+  filterText: String;
+  weatherData: WeatherData;
   geo: Coord;
 
-  constructor(private weatherSrv: WeatherService){}
+  constructor(private weatherSrv: WeatherService) {}
 
-  ngOnInit(): void
-  {
+  ngOnInit(): void {
     this.getGeoLocation();
   }
-  clickSubmit()
-  {
-    // this.weatherSrv.getWeather(this.filterText).subscribe
-    // (
-    //   data => { this.weatherData = JSON.parse(data) },
-    // );
+  clickSubmit() {
+    this.weatherSrv.getWeatherByCity(this.filterText).subscribe
+    (
+      data => 
+      {
+         this.weatherData = data 
+         document.getElementById("location").innerText =
+              this.weatherData.name + ", " + this.weatherData.sys.country;
+      },
+    );
   }
-  getGeoLocation()
-  {    
-    if (navigator.geolocation)
-    {
-      navigator.geolocation.getCurrentPosition(
-        (position)=>
-        {
-          this.geo = { lon: Math.round(position.coords.longitude), lat : Math.round(position.coords.latitude) };
-            this.weatherSrv.getWeatherOnCoords(this.geo)
-              .subscribe( data => 
-                {
-                   this.weatherData = data; 
-                   document.getElementById('location').innerText = this.weatherData.name +', '+ this.weatherData.sys.country
-                }, 
-                error => { console.log(error); }
-              );
-        });
-    }
-    else
-    {
-      console.log('Browser not support');
+  getGeoLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.geo = {
+          lon: Math.round(position.coords.longitude),
+          lat: Math.round(position.coords.latitude)
+        };
+        this.weatherSrv.getWeatherByCoords(this.geo).subscribe(
+          data => {
+            this.weatherData = data;
+            document.getElementById("location").innerText =
+              this.weatherData.name + ", " + this.weatherData.sys.country;
+          },
+          error => {
+            console.log(error);
+          }
+        );
+      });
+    } else {
+      console.log("Browser not support");
     }
   }
-  
 }
